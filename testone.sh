bec90ebe18f41dd63c135d0290ca0a1bce3de5f3
cloudymike <mikael@cloudyforecast.com>
2023-10-12 20:08:47 -0700

    testparallell is used on github

--- testone.sh
+++ testone.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 FILE=$1
-IGNOREFILES="./electrocookie/parameters.scad ./drawer/TOUL.scad"
+IGNOREFILES="./worktable/dimple_vectors.scad ./electrocookie/parameters.scad ./drawer/TOUL.scad"
+IGNOREDIRS="./Write.scad ./scad-utils"
 
 DIR="$(dirname "${FILE}")"
 BASE="$(basename "${FILE}")"
@@ -8,14 +9,22 @@ pushd ${DIR} &> /dev/null
 if echo $IGNOREFILES | grep -wq $FILE
 then
   echo "Skipping $FILE"
+elif echo $IGNOREDIRS | grep -wq $DIR
+then  
+  echo "Skipping directory $DIR"
+elif echo $DIR | grep '.git'
+then
+  echo "Skipping git hidden files"
 else
   echo "Testing $BASE in $DIR"
   STLFILE="${BASE%.*}.stl"
   time openscad -o $STLFILE --hardwarnings -D '$fn=2' $BASE
   if [ "$?" != "0" ]
   then
-      echo "FAIL: $FILE"
-      exit 1
+    echo "FAIL: $FILE"
+    exit 1
+  else
+    rm -f $STLFILE
   fi
 fi
 popd &> /dev/null
